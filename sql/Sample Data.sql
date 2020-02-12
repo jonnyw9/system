cee8df00826ae32e8002de7a58b45c3acaabae7d
Jonny <jonathan.watt@northumbria.ac.uk>
2020-02-12 16:59:10 +0000

    DB scripts

---
+++ sql/Sample Data.sql
@@ -0,0 +1,17 @@
+INSERT INTO roles
+VALUES (1, 1, 1, 1);
+
+INSERT INTO password (password_id, password)
+VALUES (1, 'password');
+
+INSERT INTO bs_user
+VALUES (1, "jonathanwatt98@outlook.com", "Jonathan", "Watt", null, 1, 1, 1);
+
+INSERT INTO roles
+VALUES (2, 0, 1, 0);
+
+INSERT INTO password (password_id, password)
+VALUES (2, 'password');
+
+INSERT INTO bs_user
+VALUES (2, "jonnysidh@gmail.com", "Jonathan", "Watt", null, 1, 2, 2);
