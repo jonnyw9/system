fd98482aa0508ece012ee33fbb2a3ea9730557d6
Jonny <jonathan.watt@northumbria.ac.uk>
2020-04-07 16:51:31 +0100

    JavaDoc, commenting and unit tests

--- src/main/java/com/tutorial/booking/system/SystemApplication.java
+++ src/main/java/com/tutorial/booking/system/SystemApplication.java
@@ -1,13 +1,19 @@
+/*
+ * Copyright (c) 2020. To JWIndustries
+ */
+
 package com.tutorial.booking.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.tutorial.booking.system.Repository")
 @EnableScheduling
+@EnableTransactionManagement
 public class SystemApplication {
 
     public static void main(String[] args) {
